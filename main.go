e86a3777e303dccc9ef298d34e0a045b8eaaea59
WeRockStar <kotchaphan.m@gmail.com>
2017-09-23 17:04:37 +0700

    Add fizzbuzz

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"strconv"
 
@@ -38,7 +39,25 @@ func main() {
 	// Routes
 	e.GET("/", hello)
 	e.GET("/add/:a/:b", add)
+	e.GET("/fizzbuzz/:number", func(c echo.Context) error {
+		number, err := strconv.Atoi(c.Param("number"))
+		if err != nil {
+			log.Fatal("Cannot convert string to int")
+		}
+		return c.JSON(http.StatusOK, map[string]string{"answer": fizzbuzz(number)})
+	})
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func fizzbuzz(number int) string {
+	if number%3 == 0 && number%5 == 0 {
+		return "FizzBuzz"
+	} else if number%3 == 0 {
+		return "Fizz"
+	} else if number%5 == 0 {
+		return "Buzz"
+	}
+	return strconv.Itoa(number)
 }
