c4b97d929364f7936d1654ac00af1cf1e639eb5f
WeRockStar <kotchaphan.m@gmail.com>
2017-09-23 16:10:40 +0700

    Add solve caesar

--- exercise.go
+++ exercise.go
@@ -1,9 +1,20 @@
 package golang
 
+import (
+	"strings"
+)
+
 func enc(xx, rot byte) byte {
-	return xx
+	if xx == 32 {
+		return xx
+	}
+	return (xx+rot)%26 + 97
 }
 
 func caesar(s string, rot byte) string {
-	return s
+	result := []string{}
+	for _, v := range s {
+		result = append(result, string(enc(byte(v), rot)))
+	}
+	return strings.Join(result, "")
 }
