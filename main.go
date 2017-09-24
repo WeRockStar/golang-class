c7f0f3b6393b692859cdb72794a227d0838ca4dd
WeRockStar <kotchaphan.m@gmail.com>
2017-09-24 14:31:06 +0700

    Add make channel

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum
-}
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
-	c := make(chan int)
+	c := make(chan int, 2)
+	c <- 1
+	c <- 2
 
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-
-	x, y := <-c, <-c
-
-	fmt.Println(x, y, x+y)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
